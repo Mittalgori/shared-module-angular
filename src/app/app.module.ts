3757a1290018b356123ee024ef79aad0b24d3816
Mittalgori <mitu796@gmail.com>
2024-05-21 08:34:33 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, SharedModule,
     AppRoutingModule
   ],
   providers: [],
